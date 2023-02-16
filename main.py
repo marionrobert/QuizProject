9ce5168f1881953e4cf2de860e4d14ec62945bc1
Marion <mrobert148@gmail.com>
2023-02-16 15:39:55 +0100

    quizbrain class (attributes and next_question method)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
     new_question = Question(question["text"], question["answer"])
     question_bank.append(new_question)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
